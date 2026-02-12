a3a21b993487140639d2125374edd5e74b883d60
Shubham Sinha <149707578+Shubhamsinha0509@users.noreply.github.com>
2026-02-12 12:02:40 +0530

    Path module

---
+++ Day-3/index.js
@@ -0,0 +1,24 @@
+const path =  require ("path")
+
+console.log("FILENAME📂", __filename)
+console.log("DIRNAME📂", __dirname)
+
+
+// 1. join 
+
+const filepath = path.join("folder","student","data.txt")
+console.log(filepath)
+
+const parsedDataPath = path.parse(filepath)
+const resolvedPath = path.resolve(filepath)
+const extname = path.extname(filepath)
+const dirname = path.dirname(filepath)
+const basename = path.basename(filepath)
+
+console.log({
+    parsedDataPath,
+    resolvedPath,
+    extname,
+    dirname,
+    basename,
+})
